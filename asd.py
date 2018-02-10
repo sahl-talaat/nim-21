a42bb217db4c93b2887b7fc4d6fa9321b50a9d96
Yasser Sobhy <yasser.sobhy.net@gmail.com>
2018-02-10 23:11:32 +0200

    initial version

---
+++ asd.py
@@ -0,0 +1,36 @@
+n = 0
+count = n + 1
+limit = 21
+
+def restart():
+    global limit, count, n
+    print('New Game!!')
+    n = 0
+    count = n + 1
+    limit = 21
+
+def substract(num, player_name):
+    global limit, count
+    limit-=num
+    count+=1
+    if limit==0:
+        print(player_name+' lossed :(')
+        restart()
+    print ("limit = ", limit)
+
+def handle_input(player_name):
+    print (player_name)
+    x = input ("x: ")
+    if x < 1 or x > 3 or x > limit:
+        print ("invalid x")
+    else:
+        substract(x, player_name)
+
+def read_user_input():
+    if count%2 == 0 :
+        handle_input("player-2")
+    else:
+        handle_input("player-1")
+
+while True:
+    read_user_input()
